80c30c0f701322c800199947d1aaf6ce83188dbf
Fabio Iemmi <fabio.iemmi@cern.ch>
2022-05-12 16:38:01 +0200

    small fix to ABCNetProducer to make it run (still throws random weights)

--- CommonTools/PileupAlgos/plugins/ABCNetProducer.cc
+++ CommonTools/PileupAlgos/plugins/ABCNetProducer.cc
@@ -62,7 +62,7 @@ ABCNetProducer::ABCNetProducer(const edm::ParameterSet& iConfig):
 {
   // Produce a ValueMap of floats linking each PF candidate with its ABCNet weight
   produces<edm::ValueMap<float> > ();
-
+  produces<pat::PackedCandidateCollection>();
 };
 
 // destructor
@@ -72,18 +72,15 @@ ABCNetProducer::~ABCNetProducer() {
 void ABCNetProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {};
 
 void ABCNetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
-  std::cout << "I AM ABCNET PRODUCER, I AM PRODUCING" << std::endl;
   // Get PFCandidate Collection
   edm::Handle<reco::CandidateView> PFCandidates;
   iEvent.getByToken(tokenPFCandidates_, PFCandidates);
   const reco::CandidateView *pfCol = PFCandidates.product();
-  
   //initialize container for ABCNet weights
   std::vector<float> weights;
   //throw random numbers in [0,1] as ABCNet weights for now
   srand(100);
   //for (unsigned int i = 0; i < pfCol->size(); i++) weights.push_back((float) rand()/RAND_MAX);
-  
   std::vector<PuppiCandidate> lCandidates; // use PuppiCandidates
   int lPackCtr = 0;
   for(auto const& aPF : *pfCol) {  
@@ -110,13 +107,15 @@ void ABCNetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) 
       lCandidates.push_back(curjet);
       lPackCtr++;
    }
-  
+  fPackedPuppiCandidates.reset( new PackedOutputCollection );
   int cCand = 0;
   for ( auto const& aCand : *PFCandidates) {
     LorentzVector pVec;
     auto const& thisCand = lCandidates[cCand];
     pVec.SetPxPyPzE(thisCand.px,thisCand.py,thisCand.pz,thisCand.e);
     std::unique_ptr<pat::PackedCandidate> pCand;
+    const pat::PackedCandidate *cand = dynamic_cast<const pat::PackedCandidate*>(&aCand);
+    pCand.reset( new pat::PackedCandidate(*cand) );
     pCand->setP4(pVec);
     fPackedPuppiCandidates->push_back(*pCand);
     cCand++;
@@ -127,7 +126,7 @@ void ABCNetProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) 
   ABCNetFiller.insert(PFCandidates,weights.begin(),weights.end());
   ABCNetFiller.fill();
   
-  iEvent.put(std::move(ABCNetOut), "weights");
+  //iEvent.put(std::move(ABCNetOut), "weights");
   iEvent.put(std::move(fPackedPuppiCandidates));
 
 }
