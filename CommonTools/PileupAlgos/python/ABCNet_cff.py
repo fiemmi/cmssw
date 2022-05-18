d4e8634d6df977c10077e34923c7f8a7cc376186
Fabio Iemmi <fabio.iemmi@cern.ch>
2022-05-18 15:23:30 +0200

    add again protobuf file and json file (disappeared from local branch for some reason?)

--- CommonTools/PileupAlgos/python/ABCNet_cff.py
+++ CommonTools/PileupAlgos/python/ABCNet_cff.py
@@ -2,6 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 abc = cms.EDProducer("ABCNetProducer",
                      candName = cms.InputTag("packedPFCandidates"),
-                     graph_path = cms.FileInPath("AttentionBasedPileupRejectionModel_Run2.pb"),
-                     preprocess_json = cms.FileInPath("preprocess.json")
+                     graph_path = cms.FileInPath("CommonTools/PileupAlgos/plugins/AttentionBasedPileupRejectionModel_Run2.pb"),
+                     preprocess_json = cms.FileInPath("CommonTools/PileupAlgos/plugins/preprocess_info.json")
 )
