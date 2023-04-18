61e6d30c31e137937f21e20cdab59667c3226d41
Fabio Iemmi <fabio.iemmi@cern.ch>
2023-04-18 18:20:01 +0200

    use latest model with dynamic 0 padding and 10 KNNs

--- CommonTools/PileupAlgos/python/ABCNet_cff.py
+++ CommonTools/PileupAlgos/python/ABCNet_cff.py
@@ -2,13 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 abc = cms.EDProducer("ABCNetProducer",
                      candName = cms.InputTag("packedPFCandidates"),
-                     graph_path = cms.FileInPath("CommonTools/PileupAlgos/data/AttentionBasedPileupRejectionModel_Run2_KDTree_dyn0pad_test.pb"),
+                     graph_path = cms.FileInPath("CommonTools/PileupAlgos/data/AttentionBasedPileupRejectionModel_Run2_KDTree_SWDMETLoss_dyn0pad_10knn.pb"),
                      preprocess_json = cms.FileInPath("CommonTools/PileupAlgos/data/preprocessing_info.json"),
                      input_tensor_name_1 = cms.string("input_1"),
                      input_tensor_name_2 = cms.string("input_2"),
                      output_tensor_name = cms.string("Identity"),
                      n_pf_cands = cms.int32(4000),
                      n_feats = cms.int32(19),
-                     n_knns = cms.int32(20),
+                     n_knns = cms.int32(10),
                      debug = cms.bool(False)
 )
