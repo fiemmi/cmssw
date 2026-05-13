bb04dc0315e67c662a9744c44d966a4b41987c30
Fabio Iemmi <fabio.iemmi@cern.ch>
2026-05-13 15:49:41 +0200

    updating computation of z for LCs in test beam
    Co-authored-by: Fabio Monti fabio.monti@cern.ch

--- Geometry/HGCalMapping/plugins/alpaka/HGCalDenseIndexInfoESProducer.cc
+++ Geometry/HGCalMapping/plugins/alpaka/HGCalDenseIndexInfoESProducer.cc
@@ -139,7 +139,14 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
                   auto position = hgcal_geom->getPosition(row.detid());
 
                   auto wafer_position = hgcal_geom->getWaferPosition(row.detid());
-		  
+
+		  //fix z coordinate of layers in setup 2
+		  if (fedRS.readoutTypes_.size()==10)
+		    row.z() = -zmap_.at(row.layer()-1);
+		  else
+		    row.z() = position.z();		    
+
+		  //fix x and y coordinates of layer 10 in setup 2
 		  if (fedRS.readoutTypes_.size()==10 && row.layer()==10) {
 		    auto shifted_x = position.x() - wafer_position.x();
 		    auto shifted_y = position.y() - wafer_position.y();
@@ -148,26 +155,29 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
 		    //rotation by 30 degrees
 		    auto rotated_x = shifted_x*cos(30./180.*M_PI) - shifted_y*sin(30./180.*M_PI);
 		    auto rotated_y = shifted_x*sin(30./180.*M_PI) + shifted_y*cos(30./180.*M_PI);
-		    GlobalPoint rotated_position(rotated_x+wafer_position.x(), rotated_y+wafer_position.y(), position.z());
+		    GlobalPoint rotated_position(rotated_x+wafer_position.x(), rotated_y+wafer_position.y(), row.z());
                     row.x() = rotated_position.x();
                     row.y() = rotated_position.y();
                     row.z() = rotated_position.z();
                     row.eta() = rotated_position.eta();
                     row.phi() = rotated_position.phi();		    
-		  }	    
+		  }
+		  //fix x and y coordinates of all other even layers
                   else if (row.layer() % 2 == 0) {
                     auto rotated_x = -position.y() + wafer_position.y() + wafer_position.x();
                     auto rotated_y = position.x() - wafer_position.x() + wafer_position.y();
-                    GlobalPoint rotated_position(rotated_x, rotated_y, position.z());
+                    GlobalPoint rotated_position(rotated_x, rotated_y, row.z());
                     row.x() = rotated_position.x();
                     row.y() = rotated_position.y();
                     row.z() = rotated_position.z();
                     row.eta() = rotated_position.eta();
                     row.phi() = rotated_position.phi();
-                  } else {
+                  }
+		  //fix x and y coordinates of odd layers 
+		  else {
                     auto rotated_x = position.y() - wafer_position.y() + wafer_position.x();
                     auto rotated_y = -position.x() + wafer_position.x() + wafer_position.y();
-                    GlobalPoint rotated_position(rotated_x, rotated_y, position.z());
+                    GlobalPoint rotated_position(rotated_x, rotated_y, row.z());
                     row.x() = rotated_position.x();
                     row.y() = rotated_position.y();
                     row.z() = rotated_position.z();
@@ -191,8 +201,8 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       edm::ESGetToken<hgcal::HGCalMappingModuleParamHost, HGCalElectronicsMappingRcd> moduleInfoTkn_;
       edm::ESGetToken<hgcal::HGCalMappingCellParamHost, HGCalElectronicsMappingRcd> cellInfoTkn_;
       edm::ESGetToken<CaloGeometry, CaloGeometryRecord> caloGeomToken_;
+      std::vector<float> zmap_ = { 0.0, -1.0, -5.0, -6.0, -9.3, -10.3, -13.8, -14.8, -17.8, -20.8 };
     };
-
   }  // namespace hgcal
 
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE
